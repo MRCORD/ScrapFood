20a1b420d1ed02efb09615af793237fce1e2b4ef
MRCORD <48929841+MRCORD@users.noreply.github.com>
2019-10-29 22:32:28 -0500

    e2

--- integrador.py
+++ integrador.py
@@ -1,1 +1,18 @@
 import pandas as pd 
+
+def info_mayosrista():
+    mayorist_data = pd.read_csv('datos.csv', sep = ';')
+    mayorist_data = mayorist_data[['Producto', 'Variedad', 'Precio MAX', 'Precio AVG', 'Precio MIN']]
+
+def info_plazaVea():
+    plaza_vea = pd.read_csv('plazavea.csv')
+    name = plaza_vea['name']
+    precio = plaza_vea['entero'] + plaza_vea['decimal']
+    plaza_vea = pd.concat([name, precio], axis=1, ignore_index=True)
+
+def info_wong():
+    wong = pd.read_csv('wong.csv', engine='python')
+    
+
+
+
