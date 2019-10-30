96de3f104f1408e6e40735ec74ce1dfe61bf641e
oscar javier Rivas Delgado <oj.rivasd@alum.up.edu.pe>
2019-10-29 23:06:40 -0500

    d

--- integrador.py
+++ integrador.py
@@ -1,18 +1,22 @@
 import pandas as pd 
 
-def info_mayosrista():
+def info_mayorista():
     mayorist_data = pd.read_csv('datos.csv', sep = ';')
     mayorist_data = mayorist_data[['Producto', 'Variedad', 'Precio MAX', 'Precio AVG', 'Precio MIN']]
+    return mayorist_data
 
 def info_plazaVea():
-    plaza_vea = pd.read_csv('plazavea.csv')
-    name = plaza_vea['name']
-    precio = plaza_vea['entero'] + plaza_vea['decimal']
-    plaza_vea = pd.concat([name, precio], axis=1, ignore_index=True)
+    plaza_vea = pd.read_csv('plazaveafinal.csv', sep = ';', engine='python')
+    return plaza_vea
 
 def info_wong():
     wong = pd.read_csv('wong.csv', engine='python')
-    
+    return wong
+
+def info_tottus():
+    tottus = pd.read_csv('tottusfinal.csv', engine='python')
+    return tottus
+
 
 
 
